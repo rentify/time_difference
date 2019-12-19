56ce527f099b91bc6990786e640510521cdce25e
Jethro Williams <jethrowilliams4@hotmail.com>
2019-12-19 10:21:42 +0000

    Add specs for leap years in_days
    Untested in_months rewrite
    
    Iron-out first bugs and get first specs passing
    
    Second wave of specs passing and bug fixes
    
    Refactor
    
    More refactor
    
    Remove commented code
    
    Add more specs for in_months
    
    Fix one failing spec and small refactor
    
    Add safeguard for if start_date.day is less than end_date.day
    
    Refactor

--- lib/time_difference.rb
+++ lib/time_difference.rb
@@ -16,7 +16,10 @@ class TimeDifference
   end
 
   def in_months
-    (@time_diff / (1.day * 30.42)).round(2)
+    @months = 0.0
+    @start_time, @end_time = @end_time, @start_time if @end_time < @start_time
+    @end_time.year - @start_time.year >= 1 ? set_multiple_year_months : set_single_year_months
+    @months.round(2)
   end
 
   def in_weeks
@@ -59,11 +62,13 @@ class TimeDifference
   private
 
   def initialize(start_time, end_time)
+    @start_time = start_time.to_time
+    @end_time = end_time.to_time
+
     start_time_in_seconds = time_in_seconds(start_time)
     end_time_in_seconds = time_in_seconds(end_time)
 
     @time_diff = (end_time_in_seconds - start_time_in_seconds).abs
-    @time_diff += 86400 if include_leap_year_day?(start_time, end_time)
   end
 
   def time_in_seconds(time)
@@ -74,10 +79,79 @@ class TimeDifference
     (@time_diff / 1.send(component)).round(2)
   end
 
-  def include_leap_year_day?(start_date, end_date)
-    start_date = start_date.to_date
-    end_date = end_date.to_date
-    (start_date..end_date).select{ |date| date.month == 2 && date.day > 28 }.any? && in_days >= 1.0
+  def set_multiple_year_months
+    first_year_months(first_year_days)
+    last_year_months(last_year_days)
+    @months += ((@end_time.year - @start_time.year) - 1) * 12 if @end_time.year - @start_time.year >= 2
   end
 
+  def first_year_months(days)
+    set_months_array(@start_time)
+    @months_array.reverse.each do |month_days|
+      break if days <= 0
+
+      days < month_days ? @months += (days / month_days) : @months += 1
+      days -= month_days
+    end
+  end
+
+  def last_year_months(days)
+    set_months_array(@end_time)
+    @months_array.each do |month_days|
+      break if days <= 0
+
+      days < month_days ? @months += (days / month_days) : @months += 1
+      days -= month_days
+    end
+  end
+
+  def first_year_days
+    (((@start_time + 1.year).at_beginning_of_year - @start_time) / 86400) - 1
+  end
+
+  def last_year_days
+    ((@end_time - @end_time.at_beginning_of_year) / 86400 + 1)
+  end
+
+  def set_single_year_months
+    @end_time.month != @start_time.month ? set_different_month_months : set_same_month_months
+  end
+
+  def set_different_month_months
+    set_months_array(@start_time)
+    @start_time.day == @end_time.day ? @months += 1 : set_partial_months
+    @months += (@end_time.month - @start_time.month) - 1
+  end
+
+  def set_partial_months
+    @end_time.day > @start_time.day ? set_partial_month_of_greater_than_one : set_partial_month_of_less_than_one
+  end
+
+  def set_partial_month_of_greater_than_one
+    @months += 1
+    @months += ((@end_time.day - @start_time.day).to_f / @months_array[@end_time.month - 1].to_f)
+  end
+
+  def set_partial_month_of_less_than_one
+    first_month_days = @months_array[@start_time.month - 1] - @start_time.day
+    @months += (first_month_days.to_f / @months_array[@start_time.month - 1].to_f)
+    @months += (@end_time.day.to_f / @months_array[@end_time.month - 1].to_f)
+  end
+
+  def set_same_month_months
+    set_months_array(@start_time)
+    @months += (@end_time.day - @start_time.day).to_f / @months_array[@start_time.month - 1.to_f]
+  end
+
+  def set_months_array(time_to_set)
+    @months_array = time_to_set.to_date.leap? ? leap_year_months : normal_year_months
+  end
+
+  def normal_year_months
+    [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  end
+
+  def leap_year_months
+    [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  end
 end
